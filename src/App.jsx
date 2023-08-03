f315f569707b8c4454eb65f72c1a539aa13c62fb
Adetola <hassannofisat017@gmail.com>
2023-08-03 09:17:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>
+          Hey there, I'm Adetola.
+        </h1>
+        <p>
+          I'm currently learning how to be a Fullstack developer and the journey has not been easy. 
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
